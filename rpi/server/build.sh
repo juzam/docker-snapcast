17668c24e28ae72bc8a83f3fecc1561fb4ceb3ed
Gio <juzam@juzam.net>
2016-09-11 15:46:26 +0200

    Initial commit

---
+++ rpi/server/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SNAP_VERSION=$1
+SNAP_ARCH=$2
+
+cp Dockerfile.template Dockerfile
+
+if [ $SNAP_ARCH != "armhf" ]; then
+
+	sed -i 's/armhf\///' Dockerfile
+fi
+
+sed -i "s/TEMPLATE_VERSION/$SNAP_VERSION/" Dockerfile
+sed -i "s/TEMPLATE_ARCH/$SNAP_ARCH/" Dockerfile
